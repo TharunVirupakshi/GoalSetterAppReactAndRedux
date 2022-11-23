bf2a983ac9faed640eeba620607ce6a7afd97b1f
ChaithanyaVR <chaithanyavr56@gmail.com>
2022-11-23 18:43:51 +0530

    Goals Controllers, Erorr handlers and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,15 @@
-console.log("hi")
-console.log("Test")
+const { application } = require('express')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5002
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+app.use('/api/goals', require('./routes/goalsRoutes'))
+
+app.use(errorHandler)
+app.listen(port, () => console.log(`Server started on ${port}`))
