d3b3d41309771f1518b98a2d4c9ae2aa77bcb2ef
ChaithanyaVR <chaithanyavr56@gmail.com>
2022-12-01 15:51:13 +0530

    JWT Auth setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,14 +33,16 @@ const registerUser = asyncHandler( async (req, res) => {
     const user = await User.create({
         name,
         email,
-        password: hashedPassword
+        password: hashedPassword,
+        
     })
 
     if(user) {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateJWT(user._id)    
         })
     } else {
         res.status(400)
@@ -66,7 +68,8 @@ const loginUser = asyncHandler( async (req, res) => {
          res.json({
             _id: user.id,
             name: user.name,
-            email: user.email 
+            email: user.email, 
+            token: generateJWT(user._id)    
          })
     } else {
         res.status(400)
@@ -78,14 +81,24 @@ const loginUser = asyncHandler( async (req, res) => {
 
 // @desc    Get User Data
 // @route   GET /api/users/me
-// @access  Public
+// @access  Private
 
 const getMe = asyncHandler( async (req, res) => {
-    res.json({
-        message: "Current User Data"
-    })
+   const {_id, name, email} = await User.findById(req.user.id)
+
+   res.status(200).json({
+    id: _id,
+    name,
+    email
+   })
 })
 
+//Generate JWT
+const generateJWT = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
 
 
 module.exports = {
