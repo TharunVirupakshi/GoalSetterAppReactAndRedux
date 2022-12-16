d2d082f965eb6f728d33625d6f630e1f074f667a
ChaithanyaVR <chaithanyavr56@gmail.com>
2022-12-16 17:54:42 +0530

    GoalForm Component UI Setup

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
-
+import GoalForm from '../components/GoalForm'
 function Dashboard() {
   const navigate = useNavigate()
   const {user} = useSelector((state) => state.auth)
@@ -12,9 +12,14 @@ function Dashboard() {
   },[user, navigate])
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+
+      <GoalForm />
+    </>
   )
 }
 
