54aff64a7f2ca5f93ff3b0a1ea080d18137b85a8
ChaithanyaVR <chaithanyavr56@gmail.com>
2022-12-17 15:58:13 +0530

    Aligned Spinner to Center

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,9 +1,17 @@
+import './Spinner.css'
 
 function Spinner() {
+
+  const style = {
+    display: "flex",
+    justifyContent: "center"}
   return (
-    <div className="loadingSpinnerConatiner">
-        <div className="loadingSpinner"></div>
-    </div>
+    <> 
+      <div className="SpinnerConatiner" style={style}>
+          <div className="loadingSpinner"></div>
+      </div>
+    </>
+   
   )
 }
 
--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -33,9 +33,24 @@ const response = await axios.get(API_URL, config)
 return response.data
 }
 
+//Delete Goal
+const deleteGoal = async(goalId, token) => {
+
+const config = {
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+}
+
+const response = await axios.delete(API_URL + goalId, config)
+
+return response.data
+}
+
 const goalService = {
     createGoal,
-    getGoals
+    getGoals,
+    deleteGoal
 }
 
 export default goalService
