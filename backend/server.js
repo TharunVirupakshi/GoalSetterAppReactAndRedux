e2103e6b39b1aca3241f39250b34d45cfd22b99d
ChaithanyaVR <chaithanyavr56@gmail.com>
2022-12-17 16:24:35 +0530

    Merged the Frontend server to Backend, Production Environment Enabled

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const { application } = require('express')
 const express = require('express')
 const colors = require('colors')
@@ -16,6 +17,15 @@ app.use(express.urlencoded({ extended: false}))
 
 app.use('/api/goals', require('./routes/goalsRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+//Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'../','frontend','build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please change the ENV to Production'))
+}
 
 app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on ${port}`))
